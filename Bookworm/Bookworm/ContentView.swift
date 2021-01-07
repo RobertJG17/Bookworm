a37e27f9d4d23172b7ec6607c562b31d0e9d3ede
RobertJG17 <robertjguerra@yahoo.com>
2021-01-07 15:04:26 -0800

    Created Fresh Project with Core Data Integration

---
+++ Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Robert Guerra on 1/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
