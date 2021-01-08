c6f81d70cc8cf87fcf837fda084c095d7939fd25
RobertJG17 <robertjguerra@yahoo.com>
2021-01-07 16:39:06 -0800

    Created Clean SwiftUI App and Began Implementing Key Core Data Infrastructure

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Student.entity(), sortDescriptors: [])
+    var students: FetchedResults<Student>
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            List {
+                ForEach(students, id: \.id) { student in
+                    Text(student.name ?? "Unknown")
+                }
+            }
+            Button("Add") {
+                let firstNames = ["Ginny", "Harry", "Hermoine", "Luna", "Ron"]
+                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+                
+                let chosenFirstName = firstNames.randomElement()!
+                let chosenLastName = lastNames.randomElement()!
+                
+                let student = Student(context: moc)
+                student.id = UUID()
+                student.name = "\(chosenFirstName) \(chosenLastName)"
+                try? moc.save()
+            }
+        }
     }
 }
 
