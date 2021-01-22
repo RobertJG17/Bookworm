b715e53eaeeae9035286c85eefdf3a31d0aeb54f
RobertJG17 <robertjguerra@yahoo.com>
2021-01-22 14:39:47 -0800

    Added Emoji Rating View for Custom Display and Created List View for Book Detail Views

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -17,19 +17,47 @@ struct ContentView: View {
     // PROPERTIES
     @State private var showingAddScreen = false
     
+    // METHODS
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            // find this book in our fetch request
+            let book = books[offset]
+            
+            // delete it from the context
+            moc.delete(book)
+        }
+        
+        // save the context
+        try? moc.save()
+    }
+    
     var body: some View {
         NavigationView {
-            Text("Count: \(books.count)")
-                .navigationBarTitle("Bookworm")
-                .navigationBarItems(trailing:
-                                        Button(action: {
-                                            self.showingAddScreen.toggle()
-                                        }) {
-                                            Image(systemName: "plus")
-                                        })
-                .sheet(isPresented: $showingAddScreen) {
-                    AddBookView().environment(\.managedObjectContext, self.moc)
+            List {
+                ForEach(books, id: \.self) { book in
+                    NavigationLink(destination: DetailView(book: book)) {
+                        EmojiRatingView(rating: book.rating)
+                            .font(.largeTitle)
+                        
+                        VStack(alignment: .leading) {
+                            Text(book.title ?? "Unknown Title")
+                                .font(.headline)
+                            Text(book.author ?? "Unknown Author")
+                                .foregroundColor(.secondary)
+                        }
+                    }
                 }
+                .onDelete(perform: deleteBooks)
+            }
+            .navigationTitle("BookWorm")
+            .navigationBarItems(leading: EditButton(), trailing: Button(action: {
+                self.showingAddScreen.toggle()
+            }) {
+                Image(systemName: "plus")
+            })
+            .sheet(isPresented: $showingAddScreen) {
+                AddBookView().environment(\.managedObjectContext, self.moc)
+            }
         }
     }
 }
---
+++ Bookworm/Bookworm/EmojiRatingView.swift
@@ -0,0 +1,33 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Robert Guerra on 1/22/21.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int16
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            return Text("1")
+        case 2:
+            return Text("2")
+        case 3:
+            return Text("3")
+        case 4:
+            return Text("4")
+        default:
+            return Text("5")
+        }
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView(rating: 3)
+    }
+}
