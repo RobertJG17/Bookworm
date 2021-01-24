f838945f6480461f61325356709814cf8cce6db8
RobertJG17 <robertjguerra@yahoo.com>
2021-01-24 15:46:34 -0800

    Finished BookWorm Implementation and Created Default Image to Account for Missing Genre.

--- Bookworm/Bookworm/DetailView.swift
+++ Bookworm/Bookworm/DetailView.swift
@@ -9,16 +9,34 @@ import SwiftUI
 import CoreData
 
 struct DetailView: View {
+    // Properties
     let book: Book
+    
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
+    @State private var showingDeleteAlert = false
+    
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery",
+                  "Poetry", "Romance", "Thriller"]
+    
+    func deleteBook() {
+        moc.delete(book)
+        
+        // uncomment this line if you want to make the deletion permanent
+        // try? self.moc.save()
+        presentationMode.wrappedValue.dismiss()
+    }
     
     var body: some View {
         GeometryReader { geometry in
             VStack {
                 ZStack(alignment: .bottomTrailing) {
-                    Image(self.book.genre ?? "Fantasy")
-                        .frame(maxWidth: geometry.size.width)
-                    
-                    Text(self.book.genre?.uppercased() ?? "FANTASY")
+                    Image(self.book.genre!.isEmpty ? "Unknown" : self.book.genre!)
+                        .resizable()
+                        .frame(maxWidth: geometry.size.width, maxHeight: geometry.size.height / 3)
+                        
+                   
+                    Text(self.book.genre!.isEmpty ? "No Genre: Photo by Matt Walsh" : self.book.genre!)
                         .font(.caption)
                         .fontWeight(.black)
                         .padding(8)
@@ -26,6 +44,7 @@ struct DetailView: View {
                         .background(Color.black.opacity(0.75))
                         .clipShape(Capsule())
                         .offset(x: -5, y: -5)
+
                 }
                 
                 Text(self.book.author ?? "Unknown author")
@@ -41,6 +60,16 @@ struct DetailView: View {
                 Spacer()
             }
         }
+        .alert(isPresented: $showingDeleteAlert) {
+            Alert(title: Text("Delete book"), message: Text("Are you sure?"), primaryButton: .destructive(Text("Delete")) {
+                self.deleteBook()
+            }, secondaryButton: .cancel())
+        }
+        .navigationBarItems(trailing: Button(action: {
+            self.showingDeleteAlert = true
+        }) {
+            Image(systemName: "trash")
+        })
         .navigationBarTitle(Text(book.title ?? "Unknown Book"), displayMode: .inline)
     }
 }
